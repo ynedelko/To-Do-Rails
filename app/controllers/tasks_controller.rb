08c7f647b855c769b3bd3cbfa459626765a77b80
Yelena Nedelko & Toma Apostolov <student@epicodus.com>
2015-10-12 16:00:50 -0700

    complete crud for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,17 @@
 class TasksController < ApplicationController
+  # def show
+  #   tasks = @list.task
+  #   render :show
+  # end
+
   def new
     @list = List.find(params[:list_id])
     @task = @list.tasks.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new(task_params)
     if @task.save
       redirect_to list_path(@task.list)
     else
@@ -9,8 +19,31 @@ class TasksController < ApplicationController
     end
   end
 
-  private
-    def task_params
-      params.require(:task).permit(:description)
+  def edit
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list= List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to list_path(@list)
+    else
+      render :edit
     end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    @task.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
